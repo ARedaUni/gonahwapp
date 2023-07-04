ec1afae09e555e8f74e0b05fc81ce227e2e620f0
amrojjeh <amrojjeh@outlook.com>
2023-07-04 03:15:14 -0500

    Fix bug where site loads too quickly for setVW

--- script.js
+++ script.js
@@ -10,17 +10,8 @@ function loadQuestions() {
     }
 }
 
-function setVW() {
-    let r = document.querySelector(":root");
-    let vw = document.body.clientWidth / 100;
-    r.style.setProperty("--vw", `${vw}px`);
-}
-
-window.addEventListener("resize", setVW);
-
 let questionHTMLs = [];
 loadQuestions();
-window.onload = setVW;
 
 const titleEl = document.getElementById("title");
 titleEl.innerText = title;;
