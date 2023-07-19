1b24eec2260872c1384ad81d19ec9ce1dd44dbf5
Amr Ojjeh <amrojjeh@outlook.com>
2023-07-18 23:14:29 -0500

    It's unofficial

---
+++ cards.js
@@ -0,0 +1,211 @@
+let cards = [
+    {
+        id: 0,
+        title: "الأربعون النووية",
+        description: "The 40 hadiths of Imam Nawawi.",
+        type: "نحو",
+        content: [
+            {
+                subid: 0,
+                title: "1 حديث",
+                sentences: []
+            },
+            {
+                subid: 1,
+                title: "2 حديث",
+            },
+            {
+                subid: 2,
+                title: "3 حديث",
+            },
+            {
+                subid: 3,
+                title: "4 حديث",
+            },
+            {
+                subid: 4,
+                title: "5 حديث",
+            },
+            {
+                subid: 5,
+                title: "6 حديث",
+            },
+            {
+                subid: 6,
+                title: "7 حديث",
+            },
+            {
+                subid: 7,
+                title: "8 حديث",
+            },
+            {
+                subid: 8,
+                title: "9 حديث",
+            },
+            {
+                subid: 9,
+                title: "10 حديث",
+            },
+            {
+                subid: 10,
+                title: "11 حديث",
+            },
+            {
+                subid: 11,
+                title: "12 حديث",
+            },
+            {
+                subid: 12,
+                title: "13 حديث",
+            },
+            {
+                subid: 13,
+                title: "14 حديث",
+            },
+            {
+                subid: 14,
+                title: "15 حديث",
+            },
+            {
+                subid: 15,
+                title: "16 حديث",
+            },
+            {
+                subid: 16,
+                title: "17 حديث",
+            },
+            {
+                subid: 17,
+                title: "18 حديث",
+            },
+            {
+                subid: 18,
+                title: "19 حديث",
+            },
+            {
+                subid: 19,
+                title: "20 حديث",
+            },
+            {
+                subid: 20,
+                title: "21 حديث",
+            },
+            {
+                subid: 21,
+                title: "22 حديث",
+            },
+            {
+                subid: 22,
+                title: "23 حديث",
+            },
+            {
+                subid: 23,
+                title: "24 حديث",
+            },
+            {
+                subid: 24,
+                title: "25 حديث",
+            },
+            {
+                subid: 25,
+                title: "26 حديث",
+            },
+            {
+                subid: 26,
+                title: "27 حديث",
+            },
+            {
+                subid: 27,
+                title: "28 حديث",
+            },
+            {
+                subid: 28,
+                title: "29 حديث",
+            },
+            {
+                subid: 29,
+                title: "30 حديث",
+            },
+            {
+                subid: 30,
+                title: "31 حديث",
+            },
+            {
+                subid: 31,
+                title: "32 حديث",
+            },
+            {
+                subid: 32,
+                title: "33 حديث",
+            },
+            {
+                subid: 33,
+                title: "34 حديث",
+            },
+            {
+                subid: 34,
+                title: "35 حديث",
+            },
+            {
+                subid: 35,
+                title: "36 حديث",
+            },
+            {
+                subid: 36,
+                title: "37 حديث",
+            },
+            {
+                subid: 37,
+                title: "38 حديث",
+            },
+            {
+                subid: 38,
+                title: "39 حديث",
+            },
+            {
+                subid: 39,
+                title: "40 حديث",
+            },
+            {
+                subid: 40,
+                title: "41 حديث",
+            },
+            {
+                subid: 41,
+                title: "42 حديث",
+            },
+            {
+                subid: 42,
+                title: "43 حديث",
+            },
+            {
+                subid: 43,
+                title: "44 حديث",
+            },
+            {
+                subid: 44,
+                title: "45 حديث",
+            },
+            {
+                subid: 45,
+                title: "46 حديث",
+            },
+            {
+                subid: 46,
+                title: "47 حديث",
+            },
+            {
+                subid: 47,
+                title: "48 حديث",
+            },
+            {
+                subid: 48,
+                title: "49 حديث",
+            },
+            {
+                subid: 49,
+                title: "50 حديث",
+            }
+        ],
+    }
+];
