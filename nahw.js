25c143f32403c35da045a1ff66b561b4ca22369b
Amr Ojjeh <amrojjeh@outlook.com>
2023-07-20 23:54:50 -0500

    Add more state classes

--- nahw.js
+++ nahw.js
@@ -1,15 +1,157 @@
 "use strict";
 
+const diacritics = {
+    "DAMMA": "\u064f",
+    "DAMMATAN": "\u064c",
+    "FATHA": "\u064e",
+    "FATHATAN": "\u064b",
+    "KASRA": "\u0650",
+    "KASRATAN": "\u064d",
+    "SUKOON": "\u0652",
+    "SHADDA": "\u0651",
+
+}
+
+function getDiacriticName(diacritic) {
+    console.assert(typeof diacritic === "string");
+    console.assert(diacritic.length === 1);
+    const index = Object.values(diacritics).indexOf(diacritic);
+    if (index === -1) return null;
+    return Object.keys(diacritics)[index];
+}
+
+function getWordEndingNameAndIndex(word) {
+    console.assert(typeof word === "string");
+    let index = word.length - 1;
+    let diacriticName = getDiacriticName(word[index]);
+    if (diacriticName === null) return {diacriticName: null, index: -1};
+    if (diacriticName === "SHADDA") {
+        index = word.length - 2;
+        diacriticName = getDiacriticName(word[index]);
+    }
+    return {diacriticName, index};
+}
+
+function getWordEndingName(word) {
+    return getWordEndingNameAndIndex(word).diacriticName;
+}
+
+function removeWordEnding(word) {
+    let index = getWordEndingNameAndIndex(word).index;
+    return word.split(word[index]).join("");
+}
+
+class WordState {
+    static flags = ["correct", "incorrect", "unattempted", "na"]
+    constructor(wordAnswer, flag="na") {
+        this.setFlag(flag);
+        if (this.getFlag() === "na") {
+            this.setFacade(wordAnswer);
+        } else {
+            this.setFacade(removeWordEnding(wordAnswer));
+        }
+        this._answerWord = wordAnswer;
+        this._answerVowel = getWordEndingName(wordAnswer);
+    }
+
+    getAnswer() {
+        return this._answerWord;
+    }
+
+    getAnswerVowelEndingName() {
+        return this._answerVowel;
+    }
+
+    getFacade() {
+        return this._facade;
+    }
+
+    setFacade(value) {
+        console.assert(typeof value === "string");
+        this._facade = value;
+    }
+
+    getFlag() {
+        return this._flag;
+    }
+
+    setFlag(value) {
+        console.assert(WordState.flags.indexOf(value) !== -1);
+        this._flag = value;
+    }
+
+    static computeFlag(wordAns) {
+        if (getWordEndingName(wordAns) == null) {
+            return "na";
+        }
+        return "unattempted";
+    }
+
+    static generateWords(sentenceAnswer) {
+        let startingIndex = 0;
+        let words = [];
+        for (let i = 1; i < sentenceAnswer.length; ++i) {
+            let c = sentenceAnswer[i];
+            if (c === ' ') {
+                let word = sentenceAnswer.substr(startingIndex, i - startingIndex);
+                words.push(new WordState(word, WordState.computeFlag(word)));
+                startingIndex = i + 1;
+                i++;
+                continue;
+            }
+            if (c === ',' || c === ':' || c === ".") {
+                let word = sentenceAnswer.substr(startingIndex, i - startingIndex);
+                words.push(new WordState(word, WordState.computeFlag(word)));
+                words.push(new WordState(c, "na"));
+                startingIndex = i + 1;
+                i++;
+                continue;
+            }
+        }
+        if (startingIndex !== sentenceAnswer.length) {
+            let word = sentenceAnswer.substr(
+                startingIndex, sentenceAnswer.length - startingIndex);
+            words.push(new WordState(word, WordState.computeFlag(word)));
+        }
+        return words;
+    }
+}
+
+class SentenceState {
+    static flags = ["correct", "incorrect", "unattempted"];
+
+    constructor(sentenceAnswer, flag="unattempted") {
+        this._words = WordState.generateWords(sentenceAnswer);
+    }
+
+    getWords() {
+        return this._words;
+    }
+
+    getAnswer(val) {
+        return this.getWords().map(x => x.getAnswer()).join(" ");
+    }
+
+    getFacade() {
+        return this.getWords().map(x => x.getFacade()).join(" ");
+    }
+
+    getFlag() {
+        return this._flag;
+    }
+
+    setFlag(value) {
+        console.assert(SentenceState.flags.some(x => x === value));
+        this._flag = value;
+    }
+}
+
 class NahwQS {
     static FLAGS = ["wrong", "correct"]
 
     constructor(answers) {
         console.assert(answers != undefined);
-        this._answers = answers;
-        this._skeletons = answers.map(x => Util.getSkeleton(x, [svowel.SHADDA]));
-        this._answerLPs = answers.map(x => Util.getLetterPacks(this.getAnswers()));
-        this._fullSkeletonText = this.getSkeletons().join(" ");
-        this._attempts = [];
+        this._sentences = answers.map(x => new SentenceState(x));
     }
 
     // Assumes letters are the same
@@ -55,28 +197,16 @@ class NahwQS {
         return this.view = new NahwQV(this);
     }
 
-    getAnswers() {
-        return this._answers;
+    getSentences() {
+        return this._sentences;
     }
 
-    getSkeletons() {
-        return this._skeletons;
+    getAnswer() {
+        return this.getSentences().map(x => x.getAnswer()).join(" ");
     }
 
-    getFullSkeletonText() {
-        return this._fullSkeletonText;
-    }
-
-    getAnswerLPs() {
-        return this._answersLPs;
-    }
-
-    getAttempts() {
-        return this._attempts;
-    }
-
-    getLastAttempt() {
-        return this.attempts[this.attempts.length - 1];
+    getFacade() {
+        return this.getSentences().map(x => x.getFacade()).join(" ");
     }
 }
 
@@ -106,7 +236,7 @@ class NahwQV {
         mainPageEl.classList.add("nahw-top-bar-fill");
         topBarEl.appendChild(mainPageEl);
 
-        for (let sentence in this.data.getSkeletons()) {
+        for (let sentence in this.data.getSentences()) {
             const sentencePageEl = document.createElement("div");
             sentencePageEl.classList.add("nahw-top-bar-page");
             sentencePageEl.classList.add("nahw-top-bar-circle");
@@ -120,9 +250,9 @@ class NahwQV {
         // Create text (TODO: Make sentence clickable)
         const textEl = this.HTML.text = document.createElement("p");
         textEl.classList.add("nahw-full-text");
-        for (let sentence of this.data.getSkeletons()) {
+        for (let sentence of this.data.getSentences()) {
             const span = document.createElement("span");
-            span.innerText = sentence;
+            span.innerText = sentence.getFacade();
             span.classList.add("nahw-full-text-sentence");
             span.setAttribute("lang", "ar");
             textEl.appendChild(span);
@@ -331,6 +461,30 @@ class NahwQV {
     }
 }
 
+// TODO: Write
+class ProgressView {
+
+}
+
+// TODO: Write
+class SentenceView {
+    constructor(sentenceText, answersText) {
+        console.assert(typeof sentenceText === "string");
+        this.sentenceText = sentenceText;
+    }
+}
+
+// TODO: Write
+class InputView {
+
+}
+
+// TODO: Write
+class ErrorView {
+
+}
+
+// TODO: Represent words instead of letters
 class Letter {
     static FLAGS = ["disabled", "normal", "wrong", "close_tanween", "close_shadda", "correct"]
 
