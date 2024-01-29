af490a816b7040d23d8f07df3005dc15a025f5fb
Amr Ojjeh <amrojjeh@outlook.com>
2024-01-28 19:03:52 -0600

    Add schema

---
+++ scripts/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS nahwapp;
+
+USE nahwapp;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hashed_password CHAR(60) NOT NULL,
+    created DATETIME NOT NULL,
+    CONSTRAINT users_uc_email UNIQUE (email)
+);
+
+-- From: https://github.com/alexedwards/scs/tree/master/mysqlstore
+CREATE TABLE IF NOT EXISTS sessions (
+    token CHAR(43) PRIMARY KEY,
+    data BLOB NOT NULL,
+    expiry TIMESTAMP(6) NOT NULL
+);
