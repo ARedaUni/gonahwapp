792a371f891aa4f098685ed19b92912d3d41c867
Amr Ojjeh <amrojjeh@outlook.com>
2024-05-07 14:39:29 -0500

    preload :)

--- ui/static/main.js
+++ ui/static/main.js
@@ -1,4 +1,5 @@
 up.link.config.followSelectors.push('a[href]')
+up.link.config.preloadSelectors.push('a[href]')
 
 up.compiler(".sentence-question>.text", function(el) {
   el.querySelector(".-active").scrollIntoView({
